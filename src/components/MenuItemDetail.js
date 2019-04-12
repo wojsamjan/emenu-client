de826b4250097b88582226cc6c026e2b10a1407b
Wojciech Januszek <wojsamjan@gmail.com>
2019-04-12 22:48:13 +0200

    Updated MenuItemDetail.js

--- src/components/MenuItemDetail.js
+++ src/components/MenuItemDetail.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 
@@ -9,17 +11,26 @@ export class MenuItemDetail extends Component {
 
   componentDidMount () {
     const { id } = this.props.match.params;
-    console.log(`MenuItemDetail: ${id}`);
+
+    axios.get(`http://127.0.0.1:8000/api/menu/menus/${id}/?format=json`)
+      .then(res => this.setState({ menu: res.data }));
   }
 
 
   render() {
+    const menu = this.state.menu;
+
     return (
       <div>
-        <Link to='/menus'>BACK</Link>
-        <h1>
-          Detail View
-        </h1>
+        <Link className='btn' to='/menus'>BACK</Link>
+        {menu ? (
+          <React.Fragment>
+            <h2>{ menu.name }</h2>
+            <p>{ menu.description }</p>
+          </React.Fragment>
+        ) : (
+          <h2>Loading...</h2>
+        )}
       </div>
     )
   }
