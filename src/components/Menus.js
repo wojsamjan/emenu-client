9d4997b908aa1e86d3ece0094f53ea36ee6e3abe
Wojciech Januszek <wojsamjan@gmail.com>
2019-04-12 15:18:00 +0200

    Added PropTypes to Menus.js

--- src/components/Menus.js
+++ src/components/Menus.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import MenuItem from './MenuItem';
+
 
 class Menus extends Component {
   render() {
     return this.props.menus.map(menu => (
-      <MenuItem menu={menu}/>
+      <MenuItem key={menu.id} menu={menu} />
     ));
   }
+}
+
+// PropTypes
+Menus.propTypes = {
+  menus: PropTypes.array.isRequired
 }
 
 export default Menus;
