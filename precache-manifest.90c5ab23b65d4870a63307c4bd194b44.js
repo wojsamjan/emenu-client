916792b9d637f16071b5b715e1fdba4545974caa
Wojciech Januszek <wojsamjan@gmail.com>
2019-04-15 01:58:07 +0200

    Updates

---
+++ precache-manifest.90c5ab23b65d4870a63307c4bd194b44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "318fac318de958b6622f",
+    "url": "/emenu-client/static/js/runtime~main.775191be.js"
+  },
+  {
+    "revision": "0ec2ac06cdacb1c214b5",
+    "url": "/emenu-client/static/js/main.fdbbe45a.chunk.js"
+  },
+  {
+    "revision": "6e83c526468c2f744390",
+    "url": "/emenu-client/static/js/2.22b84251.chunk.js"
+  },
+  {
+    "revision": "0ec2ac06cdacb1c214b5",
+    "url": "/emenu-client/static/css/main.835118c8.chunk.css"
+  },
+  {
+    "revision": "eff44376754e443232abf020daeebc8a",
+    "url": "/emenu-client/index.html"
+  }
+];
