c2ca6fd9dd8431e9bb2fc36bdd718845c6fd7a2d
Wojciech Januszek <wojsamjan@gmail.com>
2019-04-14 12:16:57 +0200

    Refacotred Pagination.js - added render helper

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -2,39 +2,21 @@ import React, { Component } from 'react'
 
 
 export class Pagination extends Component {
+  renderListItem = (key, eventHandler, text) => {
+    return (
+      <li className="btn" style={{flex: '1'}} key={key} onClick={eventHandler}>{text}</li>
+    );
+  }
+  
   render() {
     const { currentPage, lastPage, prevPage, nextPage } = this.props;
 
     return (
       <div style={{ display: 'flex' }}>
         <ul>
-          { 
-            currentPage > 1 ? 
-            <li 
-              className="btn"
-              style={{flex: '1'}}
-              key='prev' 
-              onClick={prevPage}>Prev
-            </li> 
-            : 
-            '' 
-          }
-          <li 
-            className="btn"
-            style={{flex: '1'}}
-            key='current'>{ currentPage }
-          </li>
-          { 
-            currentPage < lastPage ? 
-            <li 
-              className="btn"
-              style={{flex: '1'}}
-              key='next' 
-              onClick={nextPage}>Next
-            </li> 
-            : 
-            '' 
-          }
+          { currentPage > 1 ? this.renderListItem('prev', prevPage, 'Prev') : '' }
+          { this.renderListItem('current', null, currentPage) }
+          { currentPage < lastPage ? this.renderListItem('next', nextPage, 'Next') : ''}
         </ul>
       </div>
     )
