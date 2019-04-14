eb487d8393aaf252facd6708e773761b32e06375
Wojciech Januszek <wojsamjan@gmail.com>
2019-04-14 12:17:27 +0200

    Refacotred SortMenus.js - added render helper

--- src/components/SortMenus.js
+++ src/components/SortMenus.js
@@ -2,19 +2,18 @@ import React, { Component } from 'react'
 
 
 export class SortMenus extends Component {
+  renderHeading = (sortBy, text) => {
+    const { sortMenus } = this.props;
+    return (
+      <h2 className="btn" style={{flex: '1'}} onClick={() => sortMenus(sortBy)}>{text}</h2>
+    );
+  }
+
   render() {
     return (
       <div style={{ display: 'flex' }}>
-        <h2 
-          className="btn" 
-          style={{flex: '1'}}
-          onClick={() => this.props.sortMenus('name')}>Sortuj Nazwa
-        </h2>
-        <h2 
-          className="btn" 
-          style={{flex: '1'}}
-          onClick={() => this.props.sortMenus('meals')}>Sortuj liczba dań
-        </h2>
+        { this.renderHeading('name', 'Sortuj Nazwa') }
+        { this.renderHeading('meals', 'Sortuj Liczba Dań') }
       </div>
     )
   }
